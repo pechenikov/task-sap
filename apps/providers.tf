0c518d3cb99649becdd4fc86b551c0d82c4520f4
Dimo Pechenikov <dimo.pechenikov@gmail.com>
2025-08-12 09:06:28 +0000

    cleaning old unneeded templates and changed provider settings

--- apps/providers.tf
+++ apps/providers.tf
@@ -14,7 +14,7 @@ provider google-beta {
 }
 
 provider "kubernetes" {
-  host                   = data.google_container_cluster.cluster.endpoint
+  host                   = "https://${data.google_container_cluster.cluster.endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(data.google_container_cluster.cluster.master_auth[0].cluster_ca_certificate)
 }
